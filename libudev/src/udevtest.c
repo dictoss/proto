4ad3b71c6d93a6f51895e6da8cca4a50a75c3b1c
Norimitsu Sugimoto <dictoss@live.jp>
2016-08-11 19:49:46 +0900

    add tty. (usb 3g modem is tty subsystem)

--- libudev/src/udevtest.c
+++ libudev/src/udevtest.c
@@ -37,6 +37,7 @@ int main(int argc, char *argv[])
     udev_lookup_device("video4linux");
     udev_lookup_device("drm");
     udev_lookup_device("acpi");
+    udev_lookup_device("tty");
 
     udev_lookup_device("usb");
     
