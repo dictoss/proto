fc3b8e02205c93ef5095e0fd4ba588c9876a2310
SUGIMOTO Norimitsu <dictoss@live.jp>
2017-07-30 12:34:44 +0900

    update hello_app

--- rails-tutorial/hello_app/app/controllers/application_controller.rb
+++ rails-tutorial/hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    # render html: "hello, world!"
+    render html: "hello, mundo!"
+  end
+
+  def goodbye
+    render html: "goodbye, world!"
+  end
 end
